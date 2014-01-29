f8e31ae6069702ce7e94249deea9ed0ecb3f77e0
Hayk Karapetyan <hayk.karapetyan@inn.ru>
2014-01-29 19:52:13 +0400

    formatting

--- bible-tool/src/utils/Utils.h
+++ bible-tool/src/utils/Utils.h
@@ -1,19 +1,19 @@
-#ifndef Utils_h__
-#define Utils_h__
+	#ifndef Utils_h__
+	#define Utils_h__
 
 
-/// returns application directory
+	/// returns application directory
 wstring_t getAppDir();
 
-/// return true if the path specified by input parameter is absolute path
-/// otherwise returns false
+	/// return true if the path specified by input parameter is absolute path
+	/// otherwise returns false
 bool isAbsolutePath(const wstring_t& str);
 
-/// return true if the specified string represents an integer number
+	/// return true if the specified string represents an integer number
 bool isNumber(const wstring_t& str);
 
-/// convert string into integer number
-template <typename T>
+	/// convert string into integer number
+	template <typename T>
 inline T toNumber( const wstring_t& str ) {
 	try {
 		return boost::lexical_cast<T>(str);
@@ -24,21 +24,21 @@ inline T toNumber( const wstring_t& str ) {
 	return 0;
 }
 
-/// format conversion
+	/// format conversion
 void convertFormat( const wstring_t& uniStr, string_t& multiStr, long codePage );
 void convertFormat( const string_t& multiStr, wstring_t& uniStr, long codePage );
 
 string_t toUtf8(const wstring_t& source);
 wstring_t toUtf16(const string_t& source);
 
-/// read from specified file into buffer rawData
+	/// read from specified file into buffer rawData
 bool readFileAsBinary(const wstring_t& fileName, string_t& rawData);
 bool writeFileAsBinary(const wstring_t& fileName, string_t& rawData);
 
 wstring_t getFileName(const wstring_t& fileName);
 wstring_t getFileExt(const wstring_t& fileName);
 
-/// trim tailing and leading spaces from the string
+	/// trim tailing and leading spaces from the string
 wstring_t trim(wstring_t& str);
 
-#endif // Utils_h__
+	#endif // Utils_h__
