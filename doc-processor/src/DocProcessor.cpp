a2dce8bcd77ce90ff62241052626ccb7a014d4c8
Hayk Karapetyan <hayk.karapetyan@inn.ru>
2014-04-09 19:18:32 +0400

    minor fix

--- doc-processor/src/DocProcessor.cpp
+++ doc-processor/src/DocProcessor.cpp
@@ -17,12 +17,14 @@ using Poco::Util::HelpFormatter;
 class ChangeLoggerLevel {
 public:
     ChangeLoggerLevel(DocProcessor* dp) : dp_(dp) {
-       logInfo(dp_->logger(), makeCentralizedNote("APPLICATION STARTED", '-', 80));
+        if (dp_)
+            logInfo(dp_->logger(), makeCentralizedNote("APPLICATION STARTED", '-', 80));
     }
     ~ChangeLoggerLevel() {
-        logInfo(dp_->logger(), makeCentralizedNote("APPLICATION ENDED", '-', 80));
-        if (dp_)
+        if (dp_) {
+            logInfo(dp_->logger(), makeCentralizedNote("APPLICATION ENDED", '-', 80));
             dp_->logger().setLevel(Poco::Message::PRIO_WARNING);
+        }
     }
 
 private:
